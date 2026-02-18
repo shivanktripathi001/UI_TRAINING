7831629f7cda65e2ff597f8f6afb61c1919f51b0
shivank.tripathi <shivank.tripathi@yash.com>
2026-02-18 15:37:56 +0530

    animation_code

---
+++ JS_PRACTICE/sum.js
@@ -0,0 +1,10 @@
+
+const numbers = [10, 20, 30, 40, 50];
+
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log("Numbers:", numbers); 
+console.log("Sum:", sum); 
