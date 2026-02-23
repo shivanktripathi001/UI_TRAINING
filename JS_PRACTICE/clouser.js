a07d11042707e4ccf77620fd4e38ef0741ec7524
shivank.tripathi <shivank.tripathi@yash.com>
2026-02-23 22:23:40 +0530

    js_completed

---
+++ JS_PRACTICE/clouser.js
@@ -0,0 +1,15 @@
+function outer() {
+  let count = 0;
+
+  function inner() {
+    count++;
+    console.log(count);
+  }
+
+  return inner;
+}
+
+const counter = outer();
+counter(); // 1
+counter(); // 2
+counter(); // 3
